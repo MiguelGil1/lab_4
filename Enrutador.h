8f716c0ba455e7e30b7fd09c0857a5cf7dfd467d
MiguelGil1 <miguel.gil1@udea.edu.co>
2021-09-10 20:46:16 -0500

    Se crea la clase enrutador

---
+++ Enrutador.h
@@ -0,0 +1,34 @@
+#ifndef ENRUTADOR_H
+#define ENRUTADOR_H
+//#include "conexion.h"
+#include <vector>
+#include <iterator>
+#include <iostream>
+#include <fstream>
+#include <list>
+#include <map>
+
+using namespace std;
+
+struct Caracteristicas{
+    int costoDirecto;
+    int costo;
+    string ruta;
+};
+
+class Enrutador{
+public:
+    Enrutador(string);
+
+    string Getnombre() const;
+    void setNombre(const string &nombre);
+
+    map<string, list<Caracteristicas>> Getenrutador() const;
+    void setEnrutador(const map<string, list<Caracteristicas> > &enrutador);
+
+private:
+    map<string,list<Caracteristicas>> _enrutador;
+    string _nombre;
+};
+
+#endif // ENRUTADOR_H
